5c77bc0c1ffae09c3a77b0476efe73820a23ac34
Fighter19 <1475802+Fighter19@users.noreply.github.com>
2022-04-06 21:01:30 +0200

    Add level

--- main.cpp
+++ main.cpp
@@ -66,6 +66,105 @@ int SetupCallbacks(void)
 	return thid;
 }
 
+static size_t player_x = 5;
+static size_t player_y = 3;
+
+// Offset of the board in characters
+#define X_OFFSET 10
+#define Y_OFFSET 10
+
+#define X_SIZE 6
+#define Y_SIZE 7
+
+enum FieldPieces {
+    eGround,
+    eRock,
+    eKey,
+    eGoal,
+    eLock,
+    eEnemy,
+    eWall
+};
+
+static struct Field {
+    enum FieldPieces pieces[Y_SIZE][X_SIZE];
+} field = {
+    .pieces = {
+        {eWall,    eWall,    eWall,    eGoal,    eGround,   eWall},
+        {eWall,    eWall,    eWall,    eGround,  eLock,     eWall},
+        {eGround,  eLock,    eWall,    eRock,    eRock,     eRock},
+        
+        {eEnemy,   eRock,    eGround,  eEnemy,   eRock,     eGround},
+        {eGround,  eWall,    eEnemy,   eGround,  eGround,   eGround},
+        {eGround,  eWall,    eWall,    eGround,  eWall,     eWall},
+        {eGround,  eGround,  eGround,  eGround,  eWall,     eWall}
+    }
+};
+
+const char *ground_symbol = " ";
+const char *rock_symbol = "o";
+const char *player_symbol = "1";
+const char *goal_symbol = "4";
+const char *key_symbol = "5";
+const char *wall_symbol = "#";
+const char *lock_symbol = "L";
+const char *enemy_symbol = "E";
+
+const char* look_up_symbol(enum FieldPieces piece) {
+    const char *return_value = ground_symbol;
+    switch(piece) {
+        case eGround:
+            return_value = ground_symbol;
+            break;
+        case eRock:
+            return_value = rock_symbol;
+            break;
+        case eKey:
+            return_value = key_symbol;
+            break;
+        case eGoal:
+            return_value = goal_symbol;
+            break;
+        case eLock:
+            return_value = lock_symbol;
+            break;
+        case eEnemy:
+            return_value = enemy_symbol;
+            break;
+        case eWall:
+            return_value = wall_symbol;
+            break;
+    }
+    return return_value;
+}
+
+/** Prints the content of the field to screen */
+void printField()
+{
+    pspDebugScreenSetXY(X_OFFSET-1, Y_OFFSET-1);
+    for (size_t x = 0; x < X_SIZE+2; x++) {
+        pspDebugScreenPuts("-");
+    }
+        
+    for (size_t y = 0; y < Y_SIZE; y++) {
+        pspDebugScreenSetXY(X_OFFSET-1, Y_OFFSET+y);
+        pspDebugScreenPuts("|");
+        for (size_t x = 0; x < X_SIZE; x++) {
+            const char *symbol = look_up_symbol(field.pieces[y][x]);
+            pspDebugScreenPuts(symbol);
+        }
+        pspDebugScreenPuts("|");
+    }
+    
+    pspDebugScreenSetXY(X_OFFSET-1, Y_OFFSET+Y_SIZE);
+    for (size_t x = 0; x < X_SIZE+2; x++) {
+        pspDebugScreenPuts("-");
+    }
+    
+    pspDebugScreenSetXY(X_OFFSET+player_x, Y_OFFSET+player_y);
+    pspDebugScreenPuts(player_symbol);
+}
+
 int main(void)
 {
 	SceCtrlData pad;
@@ -83,6 +182,10 @@ int main(void)
 
 		printf("Analog X = %d ", pad.Lx);
 		printf("Analog Y = %d \n", pad.Ly);
+        
+        printField();
+        
+        pspDebugScreenSetXY(0, 3);
 
 		if (pad.Buttons != 0){
 			if (pad.Buttons & PSP_CTRL_SQUARE){
@@ -123,6 +226,7 @@ int main(void)
 			if (pad.Buttons & PSP_CTRL_RTRIGGER){
 				printf("R-trigger pressed \n");
 			}      
+
 		}
 	}
 
